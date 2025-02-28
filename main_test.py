58d414fe7256fc77034ec14678c9443af8ab33e7
RobertHimSelf <167343312+RobertHimSelf@users.noreply.github.com>
2025-02-28 16:27:00 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -4,62 +4,121 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial as poly
 
-try:
-    from .lbm_border import Circulation, Half_Way_Mirror, New_Bounce_Back
-    from .lbm_d2q9 import D2Q9_ELBM, D2Q9_LBM_BGK, D2Q9_Mixed
-    from .lbm_core import LBMCore
-    from .lbm_d2 import D2
-except ImportError:
-    from lbm_border import Circulation, Half_Way_Mirror, New_Bounce_Back
-    from lbm_d2q9 import D2Q9_ELBM, D2Q9_LBM_BGK, D2Q9_Mixed
-    from lbm_core import LBMCore
-    from lbm_d2 import D2
+_k = 2
+_b = -0.5*math.pi
 
-class elbmc(D2Q9_ELBM, Circulation): pass
+def u0(x, y):          #速度初值
+    return (
+        -math.cos(_k*x+_b)*math.sin(_k*y+_b),
+         math.sin(_k*x+_b)*math.cos(_k*y+_b))
 
-class elbmhwm(D2Q9_ELBM, Half_Way_Mirror): pass
+_kt = -2*_k**2
 
-class elbmbb(D2Q9_ELBM, New_Bounce_Back): pass
+def u(x, y, time, nu): #解析解的速度
+    that_time_thing = math.exp(_kt*nu*time)
+    ux0, uy0 = u0(x, y)
+    return ux0*that_time_thing, uy0*that_time_thing
 
-class lbmc(D2Q9_LBM_BGK, Circulation): pass
+def u_fromsolver(x, y, time, solver):
+    return u(x, y, time, solver.nu)
 
-class lbmhwm(D2Q9_LBM_BGK, Half_Way_Mirror): pass
+def Taylor_Green_compare_Irregular_Domain(solver):
+    x = np.arange(solver.shape[0], dtype=float)*solver.dx
+    y = np.arange(solver.shape[1], dtype=float)*solver.dx
+    x,y = np.meshgrid(x,y)
 
-class lbmbb(D2Q9_LBM_BGK, New_Bounce_Back): pass
+    solution_speed = solver.speed
+    
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 4))
 
-class mixhwm(D2Q9_Mixed, Half_Way_Mirror): pass
+    s1 = np.sqrt(solution_speed[:,:,0]**2+solution_speed[:,:,1]**2)
+    lines1 = ax1.streamplot(x,y,solution_speed[:,:,0].T,solution_speed[:,:,1].T,color=s1.T,cmap="cool")
+    ax1.set(xlabel=f"Numerical Solution: dx={solver.dx}",ylabel=f"time = {solver.time}")
+    fig.colorbar(lines1.lines)
+    ax1.contour(contour_x, contour_y, contour_z, levels=[0], colors="black")
 
-class mixc(D2Q9_Mixed, Circulation): pass
+    dx = solver.dx
 
-class mixbb(D2Q9_Mixed, New_Bounce_Back): pass
+    scale = math.exp(_kt*solver.nu*solver.time)
+    #scale = math.exp(-solver.time*solver.nu*8*math.pi**2)
+    precise_solution = np.array([[
+        (u0(i*dx,j*dx) if (P(i*dx, j*dx) <= 0) else (0, 0))
+        for j in range(solver.shape[1])] for i in range(solver.shape[0])])*scale
+    s2 = np.sqrt(precise_solution[:,:,0]**2+precise_solution[:,:,1]**2)
+    lines2 = ax2.streamplot(x,y,precise_solution[:,:,0].T,precise_solution[:,:,1].T,color=s2.T,cmap="cool")
+    ax2.set(xlabel=f"Precise Solution",ylabel="")
+    fig.colorbar(lines2.lines)
+    ax2.contour(contour_x, contour_y, contour_z, levels=[0], colors="black")
 
-def _test(solver: D2, dt):
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 4))
-    solver.add_flow(fig_ax=(fig, ax1))
-    t1 = int(1/16/dt)
-    solver.iter(times = t1, showlog = False, nohistory = True)
-    solver.add_flow(fig_ax=(fig, ax2))
-    t2 = t1*3
-    solver.iter(times = t2, showlog = False, nohistory = True)
-    solver.add_flow(fig_ax=(fig, ax3))
+    error = solution_speed - precise_solution
+
+    s3 = np.sqrt(error[:,:,0]**2+error[:,:,1]**2)
+    max_err = (np.abs(s3)).max()
+    lines3 = ax3.streamplot(x,y,error[:,:,0].T,error[:,:,1].T,color=s3.T,cmap="cool")
+    ax3.set(xlabel=f"Error: Max={max_err}",ylabel="")
+    fig.colorbar(lines3.lines)
+    ax3.contour(contour_x, contour_y, contour_z, levels=[0], colors="black")
 
     plt.show()
+    return max_err
 
-def Taylor_Green_test(cls, n=64):
-    m = n
-    k = 2*math.pi/n
-    ki= math.pi/n
-    dt = 1/m**2
-    solver = cls.initial(
-        np.ones((n,n)), #密度为1
-        np.array([[[
-            -math.cos(k*x+ki)*math.sin(k*y+ki),
-            math.sin(k*x+ki)*math.cos(k*y+ki)
-        ] for y in range(n)] for x in range(n)]),
-        dx=1/n, dt=dt, max_deltaH=1e-13, nu=1e-5)
-    
-    _test(solver, dt)
+#空间集合为 P(x,y) <= 0
+def P(x, y):
+    return 122 + x*(-81 + x*(49 + x*(-12 + x))) + y*(-137 + y*(84 + y*(-22 + y*2)))
+
+contour_x = np.linspace(0, 6, 1001)
+contour_y = np.linspace(0, 6, 1001)
+contour_x, contour_y = np.meshgrid(contour_x, contour_y)
+contour_z = P(contour_x, contour_y)
+
+def Taylor_Green_init_Irregular_Domain(cls, dx=1/32, nu=1/6):
+    dt = dx**2
+    c_ = (dt/dx)**2
+    #x_i = (i+0.5)*dx
+    #y_j = (j+0.5)*dx
+    n = round(6/dx) + 1   #空间为[0,6]*[0,6]集合的子集
+    speed = np.array([[
+        u0(i*dx, j*dx)
+        for j in range(n)] for i in range(n)])
+    density = np.array([[
+        1 - 3/4*c_*(math.cos(2*_k*i*dx) + math.cos(2*_k*j*dx))
+        for j in range(n)] for i in range(n)])
+    for x in range(n):
+        for y in range(n):
+            speed[x,y,:]*=density[x,y] #Change speed to solution_speed
+    solver = cls(
+        density = density,
+        momentum = speed,
+        dx=dx, dt=dt, nu=nu, p=P, u=u_fromsolver)
+    return solver
+
+def Taylor_Green_ranktest_Irregular_Domain(cls, nu=1/6):
+    dxarray = [1/4, 1/6, 1/8, 1/12, 1/16]
+    error = []
+
+    for dx in dxarray:
+        solver = Taylor_Green_init_Irregular_Domain(cls, dx=dx, nu=nu)
+        #solver.iter(1)
+        solver.iter(round(1/dx)**2//4)
+        error.append(Taylor_Green_compare_Irregular_Domain(solver))
+
+    dxarray= np.array(dxarray, dtype=float)
+    error = np.array(error)
+
+    x = np.log(dxarray)
+    y = np.log(error)
+
+    fig, ax = plt.subplots()
+    dots = ax.scatter(x, y)
+
+    p2 = poly.fit(x, y, deg=(0, 1))
+    line2 = ax.plot(x, p2(x), label=f"{p2}")
+
+    print("Rank:", (rank:=p2(1)-p2(0)))
+    plt.show()
+    return rank
 
 if __name__ == "__main__":
-    Taylor_Green_test(lbmhwm)
+    pass
